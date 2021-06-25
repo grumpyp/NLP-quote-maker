515bf927884751e5ce0a053647c9d0e84dc75edc
grumpyp <75830792+grumpyp@users.noreply.github.com>
2021-06-25 17:33:49 +0200

    Add function
    - Add a function to get instant feedback of a user input
    - This is an AJAX JS communication to get back an instant rating of the sentence the user types in
    
    WIP:
    - Connect the DB and response the top 5 matching results

--- app/views.py
+++ app/views.py
@@ -1,8 +1,10 @@
-from flask import render_template
+from re import search
+from flask import render_template, jsonify, request
 from flask import Blueprint
 from flask import Response
 from .src import functions
 from .src import db
+
 
 from bson.json_util import dumps
 
@@ -10,7 +12,16 @@ view = Blueprint("view", __name__)
 
 @view.get('/')
 def hello():
-    return 'Hello World!'
+    return render_template("index.html")
+
+@view.route('/livesearch', methods=["POST"])
+def livesearch():
+    searchbox = request.form.get("text")
+    print(searchbox)
+    rating = functions.Rating()
+    searchbox_rating = rating.sentiment_rating(searchbox)
+    result = str(searchbox_rating)    #convert json to str to try 
+    return result
 
 
 @view.get('/quotes')
