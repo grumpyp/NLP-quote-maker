40ecbae57e45b9088b2a83462cdb5c777f0d7647
grumpyp <75830792+grumpyp@users.noreply.github.com>
2021-07-19 22:36:00 +0200

    test
    test

--- app/src/db.py
+++ app/src/db.py
@@ -13,9 +13,9 @@ from bson.objectid import ObjectId
 
 from app import config
 
-URL = os.environment['secrets.mongodb_url']
-PW = os.environment['secrets.mongodb_user']
-Usr = os.environment['secrets.mongodb_password']
+URL = os.environ['secrets.mongodb_url']
+PW = os.environ['secrets.mongodb_user']
+Usr = os.environ['secrets.mongodb_password']
 
 logger = logging.getLogger(__name__)
 
