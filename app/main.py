76be111a403682f902de64baf8fd0ca306235d0d
grumpyp <75830792+grumpyp@users.noreply.github.com>
2021-07-19 20:20:47 +0200

    debug
    sd

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from .views import view
-import .config
+from .config import log_file, production
 
 import src.functions 
 
 import logging
 
 logging.basicConfig(
-    filename=config.log_file,
+    filename=log_file,
     filemode="w",
     format="%(name)s - %(levelname)s - %(message)s",
     level=logging.DEBUG,
@@ -20,6 +20,6 @@ app = Flask(__name__)
 
 app.register_blueprint(view)
 
-if __name__ == '__main__' and config.production == True:
+if __name__ == '__main__' and production == True:
     logger.debug("Going to start app ")
     app.run()
